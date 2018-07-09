61ca724beebd068aa6a7316bfd45d2bb37065696
fanjianzhou <fanjianzhou@dyrs.com.cn>
2018-07-09 16:10:00 +0800

    '第一次提交代码'

---
+++ src/app.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <div id="sss">{{text}}</div>
+</template>
+
+<script>
+export default{
+ data(){
+     return {
+         text:'xxxx',
+     }
+ },
+
+}
+
+</script>
+
+<style>
+ #sss{
+     color:red;
+ }
+</style>
